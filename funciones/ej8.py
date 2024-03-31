aaf66c654fb21734c53f165a7ef4c4ae11162dc9
Jacintotatu <jacintoconesa@hotmail.com>
2024-04-01 01:07:01 +0200

    hecho con genesis

--- funciones/ej8.py
+++ funciones/ej8.py
@@ -1,18 +1,56 @@
+"""
+disco = {}
+
 def hacer_album(artista, album, canciones=None):
     disco = {'artista' : artista, 'album' : album}
 
+    if canciones:
+        disco['Cancion']=can
+    return disco
+
 while True:
-    art = input('Artista: ')
+    print('Pon q para quitar esto.')
     disco['artista']=art
+    art = input('Artista: ')
+    if art == 'q':
+        print('Adios')
+        break
     alb = input('Album: ')
     disco['album']=alb
-    can = input('Canciones: ')
-    if canciones!=None:
-        disco['Tracks']=canciones
-    elif art == 'q' or alb == 'q' or can == 'q':
+    if alb == 'q':
+        print('Adios')
+        break
+    can = input('Canción: ')
+    if can == 'q':
+        print('Adios')
         break
 
-    print(hacer_album(art, alb, can))
+    
+lp = hacer_album(art, alb, can)
+
+print(lp)
+"""
+
+def hacer_album(artista, album, canciones=None):
+    disco = {'artista': artista, 'cd': album}
+    if canciones:
+        disco['canciones'] = canciones
+    return disco
+
+while True:
+    artista = input("Introduce el nombre del artista (o escribe 'salir'): ")
+    if artista.lower() == "salir":
+        break
+    album = input("Introduce el título del álbum: ")
+    canciones = input("Introduce las canciones del álbum (separadas por comas): ")
+    if canciones:
+        canciones = canciones.split(",")
+    album_creado = hacer_album(artista, album, canciones)
+    print(f"\nÁlbum creado: {album_creado}")
+
+print("¡Gracias por usar el creador de álbumes!")
+
+
 
     
     
